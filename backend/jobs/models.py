b01ca1c31ac3f87c785e8bff0ec5865641bb4ea9
Dixie Korley <dixiekorley@gmail.com>
2018-09-11 13:14:13 -0400

    started jobs app, commented plan for models

---
+++ backend/jobs/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+"""
+Job Post
+
+ID (Integer)
+Company Name (String)
+Description (String)
+Salary (Float)
+Category/Job Roles (ID)
+Requirements (String)
+Image (Profile Picture)
+Tags (ID)
+Is_Active
+Job Location
+Does it require a degree?
+
+
+"""
+
+"""
+Employee
+
+Profile Picture
+Email
+First Name
+Last Name
+Company Name
+Summary
+Applications Inbox
+Password
+Can they post a job? NO
+
+"""
+
+"""
+
+Employer
+
+Profile Picture
+Email
+First Name
+Last Name
+Company Name
+Summary
+Applications Inbox
+Password
+Can they post a job? YES
+
+"""
