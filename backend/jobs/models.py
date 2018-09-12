eb1140d107ff6426ee9b60a507618c0f671f95a6
holly-g <hollygiang@penguin>
2018-09-12 17:59:45 +0000

    Add taggit and move secret key out of settings

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,20 +1,45 @@
 from django.db import models
 from django.utils import timezone
-from django import forms
+from django.contrib.auth.models import User
+
+from uuid import uuid4 
+
+from taggit.managers import TaggableManager
+
+# Employer model
+class Employer(models.Model):
+    company_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.FileField(upload_to='post_image', blank=True)
+    email = models.CharField(max_length=35)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    summary = models.TextField()
+    applicationInbox = models.CharField(max_length=35)
+    password = models.CharField(max_length=100, default="", null=False)
+    isEmployee = models.BooleanField(default=False)
+    isActive = models.BooleanField()
+
 
 class JobPost(models.Model):
+    #id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     company_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.FileField(upload_to='post_image', blank=True)
+    company_image = models.ImageField(upload_to='post_image', blank=True)
+    job_location = models.CharField(max_length=30, blank=True)
+    requirements = models.CharField(max_length=400, blank=True)
     salary = models.IntegerField()
     is_active = models.BooleanField()
+    tags = TaggableManager()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    
+    class Meta:
+        ordering = ('published_date',)
 
     def __str__(self):
         return self.title 
