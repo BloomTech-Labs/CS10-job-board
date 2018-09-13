1e9a967c8ce64c0f36dba681c12e4ed0277c77b5
sagardesai90 <saagardesai@gmail.com>
2018-09-12 19:39:36 -0500

    added ability to create and save new employers and employees, including ability to add images

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import JobPost
+from .models import JobPost, Employee, Employer
 
 admin.site.register(JobPost)
+admin.site.register(Employee)
+admin.site.register(Employer)
