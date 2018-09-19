b392ff29b012ed29d89548baacc7071b69863850
masontmorrow <masontmorrow@gmail.com>
2018-09-19 12:53:48 +0200

    Typo fix

--- frontend/src/components/utility/NoMatch.js
+++ frontend/src/components/utility/NoMatch.js
@@ -6,7 +6,7 @@ const NoMatch = () => {
     return (
         <div className="no-match">
             <h2>Woopsies!</h2>
-            <h3>The page you are looking for does not exit.</h3>
+            <h3>The page you are looking for does not exist.</h3>
             <Link to="/">Home</Link>
         </div>
     );
