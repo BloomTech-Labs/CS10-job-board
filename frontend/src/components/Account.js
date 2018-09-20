c9fd6f31ef461d5d06ebbfca57c3a94b8a7728c2
masontmorrow <masontmorrow@gmail.com>
2018-09-21 01:56:59 +0200

    Add Account.js & styling

---
+++ frontend/src/components/Account.js
@@ -0,0 +1,63 @@
+import React from "react";
+import axios from "axios";
+import '../css/Account.css';
+
+class Account extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: null,
+            account: {
+                email: `test@test.com`,
+                first_name: `John Smith`
+            }
+        }
+    }
+
+    componentDidMount() {
+        // fetch account details and mount on state using axios.get()
+    }
+
+    onChange = e => {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    // needs account PUT for updates to info handler
+    handleAccountUpdate = e => {
+        e.preventDefault();
+        // axios.put()
+    }
+
+    // needs password PUT to verify and update password handler
+    handlePasswordUpdate = e => {
+        e.preventDefault();
+        // axios.put()
+    }
+
+    render() {
+        const { account } = this.state;
+        return (
+            <div className="account">
+                <form className="account-form">
+                    <label htmlFor="">email:</label>
+                    <input type="text" value={account.email} onChange={this.onChange}/>
+                    <label htmlFor="">First Name</label>
+                    <input type="text" value={account.first_name} onChange={this.onChange}/>
+                    <button onClick={this.handleAccountUpdate}>Save</button>
+                </form>
+                <form className="password-reset">
+                    <label htmlFor="">Old Password</label>
+                    <input type="text" onChange={this.onChange}/>
+                    <label htmlFor="">New password:</label>
+                    <input type="text" onChange={this.onChange}/>
+                    <label htmlFor="">Confirm password:</label>
+                    <input type="text" onChange={this.onChange}/>
+                    <button onClick={this.handlePasswordUpdate}>Update</button>
+                </form>
+            </div>
+        );  
+    }
+}
+
+export default Account;
