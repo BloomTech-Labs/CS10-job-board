b719b7e04135489c5d55476c63ec2f1292790041
sagardesai90 <saagardesai@gmail.com>
2018-09-21 14:53:00 -0500

    added UserMembership class and method to link users to Stripe ID, and Subscription class to define which subscription a user currently has

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from jobs.models import JobPost, Employee, Employer, User, Membership
+from jobs.models import JobPost, Employee, Employer, User, Membership, UserMembership, Subscription
 
 admin.site.register(JobPost)
 admin.site.register(Employee)
 admin.site.register(Employer)
 admin.site.register(User)
 admin.site.register(Membership)
+admin.site.register(UserMembership)
+admin.site.register(Subscription)
