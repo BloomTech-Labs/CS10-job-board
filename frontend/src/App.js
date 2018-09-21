bf073bc73e055dee67673d7030aa7f63d35392e4
masontmorrow <masontmorrow@gmail.com>
2018-09-21 02:05:47 +0200

    Add login/logout handling & Routes:
    - logIn(): controls navigation component appearance
    - handleLogout(): removes token & redirects
    - Add application routes
    - export App using withRouter from react-router-dom to access history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { Landing, NoMatch } from "./components";
+import { Route, Switch, NavLink, withRouter } from "react-router-dom";
+import { Account, Billing, Dashboard, Job, JobList, Landing, Navigation, NoMatch } from "./components";
 import './css/App.css';
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       loggedIn: false
     }
@@ -15,20 +15,42 @@ class App extends React.Component {
     const token = localStorage.getItem('token');
     if (token) {
       this.setState({ loggedIn: true });
+      this.props.history.push('/jobs');
     }
+  }
+
+  logIn = () => {
+    this.setState({ loggedIn: true});
+  }
+
+  handleLogout = e => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    this.setState({ loggedIn: false });
+    this.props.history.push('/');
   }
 
   render() {
     const { loggedIn } = this.state;
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route component={NoMatch} />
-        </Switch>
+        {loggedIn ? (
+          <Navigation handleLogout={this.handleLogout}/>
+        ) : (null)}
+        <div className="main">
+          <Switch>
+            <Route exact path="/" render={props => (<Landing logIn={this.logIn}/>)} />
+            <Route path="/jobs" component={JobList} />
+            <Route path="/account" component={Account} />
+            <Route path="/billing" component={Billing} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/job/:id" component={Job} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
