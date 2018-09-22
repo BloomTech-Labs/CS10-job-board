6e080f98c286b9a5a72187de15ea3fc43c089027
masontmorrow <masontmorrow@gmail.com>
2018-09-22 02:42:09 +0200

    Modify Allowed_Hosts

--- jobsboard/settings.py
+++ jobsboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'https://sharp-bhabha-303aff.netlify.com']
+ALLOWED_HOSTS = ['job-board-backend.herokuapp.com', '127.0.0.1']
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
