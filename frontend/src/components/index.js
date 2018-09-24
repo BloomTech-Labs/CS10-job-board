a2602809458cd5b279db59a1097c9d74808bd3c6
holly-g <hollygiang@penguin>
2018-09-24 14:57:45 +0000

    Add tags to index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,6 @@
-export { default as  Landing } from "./Landing.js";
+export { default as Landing } from "./Landing.js";
 export { default as Login } from "./auth/Login.js";
 export { default as Register } from "./auth/Register.js";
 export { default as NoMatch } from "./utility/NoMatch.js";
+export { default as PostJob } from "./PostJob.js";
+export { default as Tags } from "./Tags.js";
