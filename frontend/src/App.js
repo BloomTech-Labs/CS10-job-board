5613068884de6410c1033a2eeff236173efb460b
masontmorrow <masontmorrow@gmail.com>
2018-09-25 02:18:29 +0200

    Remove NavLink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, NavLink, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import  PostJob  from './components/PostJob';
 import { Account, Billing, Dashboard, Job, JobList, Landing, Navigation, NoMatch } from "./components";
 import './css/App.css';
