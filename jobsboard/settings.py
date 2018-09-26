60b3f3adf935358ca17ae26bbf80ab2fa5a88371
masontmorrow <masontmorrow@gmail.com>
2018-09-27 00:42:08 +0200

    Reconfigure settings.py:
    - Remove AllowAny from Permission_Classes
    - Remove TokenAuthentication from Authentication_Classes

--- jobsboard/settings.py
+++ jobsboard/settings.py
@@ -156,13 +156,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ),
 }
 
