aca5b90cadff43a43765c5dbeaa1fcc883cb149c
masontmorrow <masontmorrow@gmail.com>
2018-09-30 04:01:30 +0200

    Add Dropdown UI for nav-bar & styling

--- frontend/src/components/utility/Navigation.js
+++ frontend/src/components/utility/Navigation.js
@@ -1,22 +1,47 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "antd";
+import { Menu, Dropdown, Button, Icon } from "antd";
 import "../../css/Navigation.css";
 import { Search } from "../";
 
 const Navigation = props => {
     const { employer } = props;
+    // const menuStyle = {
+    //     ""
+    // }
+    const menu = (
+        <Menu>
+            <Menu.Item key="0">
+                <Icon type="user"/>
+                <NavLink to='/account'>Account</NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="1">
+                <Icon type="dollar"/>
+                <NavLink to='/billing'>Billing</NavLink>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="3">
+                <Icon type="poweroff"/>
+                <NavLink to='/' onClick={props.logOut}>Logout</NavLink>
+            </Menu.Item>
+        </Menu>
+        );
+
     return (
         <div className="nav-bar">
             <NavLink to='/jobs'>Jobs</NavLink>
             <NavLink to='/dashboard'>Dashboard</NavLink>
-            <NavLink to='/account'>Account</NavLink>
-            <NavLink to='/billing'>Billing</NavLink>
+            <div className="whitespace"></div>
             <Search />
             {employer ? (
-                <NavLink to='/addjob'><Button>Add Job</Button></NavLink>
+                <NavLink to='/addjob'><Button>Post a Job</Button></NavLink>
                 ) : (null)}
-            <NavLink to='/' onClick={props.logOut}>Logout</NavLink>
+            <Dropdown overlay={menu} trigger={['hover']} placement="bottomRight">
+                <a className="ant-dropdown-link" href="#">
+                  <Icon type="setting" style={{"margin-top": "6px"}}/>
+                </a>
+            </Dropdown>
         </div>
     );
 }
