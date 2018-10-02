62c4c98471a19a6d4c57c858cb9e26221cec7b64
Dixie Korley <dixiekorley@gmail.com>
2018-10-02 17:00:34 -0400

    run pytest - error

--- jobs/tests/models_test.py
+++ jobs/tests/models_test.py
@@ -1,13 +1,12 @@
+
 from mixer.backend.django import mixer
 import pytest
 
 
 @pytest.mark.django_db
 class ModelsTest:
+    # test cases for JobPost model
     def publish_class(self):
         jobpost = mixer.blend('jobs.JobPost', quantity=1)
         assert jobpost.publish == True
-        
-    def str_title(self):
-        jobpost = mixer.blend('jobs.JobPost', quantity=1)
-        assert jobpost._str_ == self.title
+
