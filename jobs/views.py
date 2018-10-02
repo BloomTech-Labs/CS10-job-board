f7f01fb62b673658fbe4651c3d06ed13b42b2656
holly-g <hollygiang@penguin>
2018-10-02 21:55:28 +0000

    Create view to send hello email

--- jobs/views.py
+++ jobs/views.py
@@ -1,15 +1,22 @@
+import os
 import uuid
+from django.conf import settings 
+from django.http import HttpResponse
+
+import sendgrid
+from sendgrid.helpers.mail import *
+#from django.core.mail import send_mail
 from djoser.views import UserView, UserDeleteView
 from djoser import serializers
-from rest_framework import views, permissions, status
-from rest_framework.response import Response
-from rest_framework import permissions
-from .models import User, JobPost, Membership, UserMembership
 from rest_framework import views, permissions, status, generics
 from rest_framework.response import Response
-from django.shortcuts import render
-from django.utils import timezone
+from rest_framework import permissions
 from django.views.generic import ListView
+from rest_framework.response import Response
+from .models import User, JobPost, Membership
+
+
+# import JobPost serializer
 from .api import JobPostSerializer, JobPreviewSerializer
 
 
@@ -30,41 +37,31 @@ class ListJobPost(generics.ListAPIView):
 
 
 class CreateJobPost(generics.CreateAPIView):
-
-    def post(self, request):
-        if request.data['is_active'] is True:
-            # If published when created,
-            # add published_date to request data to then be serialized
-            request.data['published_date'] = timezone.now()
-            serializer_class = JobPostSerializer(data=request.data)
-            serializer_class.is_valid()
-            serializer_class.save()
-            return Response(status=status.HTTP_201_CREATED)
-        else:
-            serializer_class = JobPostSerializer(data=request.data)
-            serializer_class.save()
-            return Response(status=status.HTTP_201_CREATED)
+    serializer_class = JobPostSerializer
 
 
 class DetailJobPost(generics.RetrieveUpdateDestroyAPIView):
     queryset = JobPost.objects.all()
     serializer_class = JobPostSerializer
 
-
-def get_user_membership(request):
-    user_membership_qs = UserMembership.objects.filter(user=request.user)
-    if user_membership_qs.exists():
-        return user_membership_qs.first()
-    return None
-
 # for selecting a paid membership
 class MembershipSelectView(ListView):
     # model = Membership
-    # queryset = JobPost.objects.all()
-    # serializer_class = JobPostSerializer
+    queryset = JobPost.objects.all()
+    serializer_class = JobPostSerializer
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
-        current_membership = get_user_membership(self.request)
-        context['current_membership'] = str(current_membership.membership)
-        return context
+def contact(request):
+    sg = sendgrid.SendGridAPIClient(
+        apikey=os.environ.get('SENDGRID_API_KEY')
+    )
+    from_email = Email('test@example.com')
+    to_email = Email('cs10jobboard@gmail.com')
+    subject = 'Testing!'
+    content = Content(
+        'text/plain',
+        'hello, world'
+    )
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+
+    return HttpResponse('Email Sent!')
