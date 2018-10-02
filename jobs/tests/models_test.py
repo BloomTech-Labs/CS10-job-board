64170eb270f2a8b564edeb2d907b4db3893cde36
Dixie Korley <dixiekorley@gmail.com>
2018-10-02 15:26:15 -0400

    touch test file for views, create two test cases for job post model

--- jobs/tests/models_test.py
+++ jobs/tests/models_test.py
@@ -4,6 +4,10 @@ import pytest
 
 @pytest.mark.django_db
 class ModelsTest:
-    def test(self):
+    def publish_class(self):
         jobpost = mixer.blend('jobs.JobPost', quantity=1)
         assert jobpost.publish == True
+        
+    def str_title(self):
+        jobpost = mixer.blend('jobs.JobPost', quantity=1)
+        assert jobpost._str_ == self.title
