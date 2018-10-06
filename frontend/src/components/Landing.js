765f7a010ab8f869f98ee6936505ae7261b5afee
masontmorrow <masontmorrow@gmail.com>
2018-10-07 00:07:55 +0200

    Remove fetchJobs from Landing.js & add styling classes:
    1. Add landing classes to render elements

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Login, Register } from './';
-import axios from 'axios';
-import { Alert } from 'antd';
-import { withRouter } from 'react-router-dom';
-import hero from '../assets/hero.svg';
+import { Button } from 'antd';
+import { withRouter, NavLink } from 'react-router-dom';
 import '../css/Landing.css';
 
 class Landing extends React.Component {
@@ -14,59 +12,33 @@ class Landing extends React.Component {
         };
     }
 
-    componentDidMount() {
-        // Prevents calling a GET request every time component is rendered
-        // jobs is inherited from App.js
-        if (!this.props.jobs) {
-            this.fetchJobs();
-        }
-    }
-
     changeComponent = () => {
         this.setState({ login: !this.state.login });
-    };
-
-    fetchJobs = () => {
-        this.setState({ loading: true });
-        const token = localStorage.getItem('token');
-        const requestOptions = { headers: { Authorization: `JWT ${token}` } };
-        axios
-            .get(
-                `${process.env.REACT_APP_API}${
-                    this.props.history.location.pathname
-                }`,
-                requestOptions
-            )
-            .then(response => {
-                // setJobs is inherited from App.js
-                this.props.setJobs(response.data);
-            })
-            .catch(err => {
-                this.setState({
-                    error: `Error processing request. Try Again.`
-                });
-            });
-        this.setState({ loading: false });
     };
 
     render() {
         const { login, error, loading } = this.state;
         return (
-            <div className="landing-page">
+            <div className="landing">
                 <div className="hero-div">
-                    <h1>Open Jobs</h1>
-                    <h2>
+                    <h1 className="landing-title">Open Jobs</h1>
+                    <h2 className="landing-copy">
                     No Degree, No Problem.<br />Your next job is just a click
                     away.
                     </h2>
                 </div>
-                {/* <img src={hero} draggable={false} alt="hero illustration" /> */}
+
                 {/* login toggles the display of Login or Register components */}
+
                 <div>
                 {login ? (
-                    <Login {...this.props} />
+                    <div className="landing-form">
+                        <Login {...this.props} />
+                    </div>
                 ) : (
-                    <Register {...this.props} />
+                    <div className="landing-form">
+                        <Register {...this.props} />
+                    </div>
                 )}
                 <a onClick={this.changeComponent}>
                     {login ? `Register` : `Login`}
