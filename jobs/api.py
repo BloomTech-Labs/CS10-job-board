980cf166ddefaef2f94de116724dcce68275976a
masontmorrow <masontmorrow@gmail.com>
2018-10-10 23:36:24 +0200

    Remove commented code

--- jobs/api.py
+++ jobs/api.py
@@ -1,15 +1,9 @@
-# from django.contrib.auth.models import User
-# from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-# from django.core import serializers
-# from django.conf import settings, urls
-from django.utils import timezone
-
 from .models import JobPost, User, UserMembership, Membership, Payment
 from rest_framework import serializers
 from taggit_serializer.serializers import (TagListSerializerField, TaggitSerializer)
 
  
-# Serializers for API representation 
+# Serializers for API representation
 
 class JWTSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,19 +19,7 @@ class UserIDSerializer(serializers.ModelSerializer):
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
 
-    # def create(self, validated_data):
-    #     user = User(
-    #         email=validated_data['email'],
-    #         password=validated_data['password'],
-    #         # is_employer=request.is_employer,
-    #         # first_name=request.first_name,
-    #         # last_name=request.last_name
-    #     )
-    #     user.set_password(validated_data['password'])
-    #     user.save()
-    #     return user
-
-    # Encrypts password with create_user=Django default create user method 
+    # Encrypts password with create_user=Django default create user method
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
@@ -55,25 +37,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 class JobPostSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
-    # company = UserIDSerializer()
-
-    # def create(self, validated_data):
-    #     company = self.context['request'].user
-    #     request = self.context['request']
-    #     # is_active = hasattr(request.data, 'is_active')
-    #     is_active = request.data['is_active']
-    #     print(is_active, request.data)
-
-    #     if request.data['is_active'] is True:
-    #         request.data['published_date'] = timezone.now()
-    #         print(request.data['published_date'])
-
-    #     job_post = JobPost.objects.create(
-    #         company=company,
-    #         published_date=published_date,
-    #         **validated_data
-    #     )
-    #     return job_post
 
     class Meta:
         model = JobPost
