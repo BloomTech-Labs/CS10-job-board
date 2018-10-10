86cc7e635d780bf87aa39d93403df9b1125b6766
masontmorrow <masontmorrow@gmail.com>
2018-10-10 20:35:54 +0200

    Changed index.js import names & added sections to match folder structure

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,32 +1,30 @@
-// auth
-export { default as Landing } from "./Landing.js";
-export { default as CompanyLanding } from "./CompanyLanding.js";
-export { default as Login } from "./auth/Login.js";
-export { default as Register } from "./auth/Register.js";
-export { default as CompanyRegister } from "./auth/CompanyRegister.js";
-// utility
-export { default as Navigation } from "./utility/Navigation.js";
-export { default as NoMatch } from "./utility/NoMatch.js";
-export { default as Search } from "./utility/Search.js";
+// Auth
+export { default as CompanyLanding } from './auth/CompanyLanding.js';
+export { default as CompanyRegister } from './auth/CompanyRegister.js';
+export { default as Landing } from './auth/Landing.js';
+export { default as Login } from './auth/Login.js';
+export { default as Register } from './auth/Register.js';
 // Billing
-export { default as Billing } from "./billing/Billing.js";
-export { default as CheckoutForm } from "./billing/CheckoutForm.js";
-//Dashboard Components
-export { default as PostedJobs } from "./dashboard/PostedJobs.js";
-export { default as PostedPreview } from "./dashboard/PostedPreview.js";
-export { default as JobPostCounter } from "./dashboard/JobPostCounter.js";
-
-// root
-export { default as Account } from "./Account.js";
-export { default as CompanyDashboard } from "./CompanyDashboard.js";
-export { default as Dashboard } from "./Dashboard.js";
-export { default as Job } from "./Job.js";
-export { default as JobList } from "./JobList.js";
-export { default as JobPreview } from "./JobPreview.js";
-export { default as TagView } from "./TagView.js";
-// antd components
-export { default as JobPost } from "./JobPost.js";
-export { default as TagCreate } from "./TagCreate.js";
-export { default as CompanyProfile } from "./CompanyProfile";
-export { default as Avatar } from "./Avatar";
+export { default as Billing } from './billing/Billing.js';
+export { default as CheckoutForm } from './billing/CheckoutForm.js';
+// Company
+export { default as CompanyDashboard } from './company/CompanyDashboard.js';
+export { default as CompanyJobCounter } from './company/CompanyJobCounter.js';
+export { default as CompanyJobList } from './company/CompanyJobList.js';
+export { default as CompanyProfile } from './company/CompanyProfile.js';
+// Job
+export { default as Job } from './job/Job.js';
+export { default as JobList } from './job/JobList.js';
+export { default as JobPost } from './job/JobPost.js';
+export { default as JobPreview } from './job/JobPreview.js';
+export { default as TagCreate } from './job/TagCreate.js';
+export { default as TagView } from './job/TagView.js';
+// Seeker
+export { default as Account } from './seeker/Account.js';
+export { default as Dashboard } from './seeker/Dashboard.js';
+// Utility
+export { default as Avatar } from './utility/Avatar.js';
+export { default as Navigation } from './utility/Navigation.js';
+export { default as NoMatch } from './utility/NoMatch.js';
+export { default as Search } from './utility/Search.js';
 
