990031bfa725b7ad569f8cc487c5bd4ecf8d5ec0
masontmorrow <masontmorrow@gmail.com>
2018-10-12 04:34:09 +0200

    Move CompanyJobCounter inside of CompanyJobList

--- frontend/src/components/company/CompanyDashboard.js
+++ frontend/src/components/company/CompanyDashboard.js
@@ -16,7 +16,6 @@ class CompanyDashboard extends Component {
                 <div className="row">
                     <div className="col s12 m6"></div>
                     <div className="col s12 m5 offset-m1"></div>
-                    <CompanyJobCounter />
                     <CompanyJobList />
                 </div>
             </div>
