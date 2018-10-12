6ef9a488953df1d3c440d7ced46299e259dae4f2
masontmorrow <masontmorrow@gmail.com>
2018-10-12 02:45:37 +0200

    Add optional jwt_config.py (not working):
    - For later config of JWT payload

--- jobs/jwt_config.py
+++ jobs/jwt_config.py
@@ -2,34 +2,35 @@ from datetime import datetime
 from calendar import timegm
 from rest_framework_jwt.settings import api_settings
 
-# Reference: https://github.com/GetBlimp/django-rest-framework-jwt/issues/145#issuecomment-249349031
+# To fully customize JWT returned payload:
+#   Reference -> https://github.com/GetBlimp/django-rest-framework-jwt/issues/145#issuecomment-249349031
 
 
-def jwt_get_secret_key(user):
-    return user.jwt_secret
+# def jwt_get_secret_key(user):
+#     return user.jwt_secret
 
 
-def jwt_payload_handler(user):
-    """ Custom payload handler
-    Token encrypts the dictionary returned by this function, and can be decoded by rest_framework_jwt.utils.jwt_decode_handler
-    """
-    return {
-        'user_id': user.pk,
-        'is_employer': user.is_employer,
-        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA,
-        'orig_iat': timegm(
-            datetime.utcnow().utctimetuple()
-        )
-    }
+# def jwt_payload_handler(user):
+#     """ Custom payload handler
+#     Token encrypts the dictionary returned by this function, and can be decoded by rest_framework_jwt.utils.jwt_decode_handler
+#     """
+#     return {
+#         'user_id': user.pk,
+#         'is_employer': user.is_employer,
+#         'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA,
+#         'orig_iat': timegm(
+#             datetime.utcnow().utctimetuple()
+#         )
+#     }
 
 
-def jwt_response_payload_handler(token, user=None, request=None):
-    """ Custom response payload handler.
+# def jwt_response_payload_handler(token, user=None, request=None):
+#     """ Custom response payload handler.
 
-    This function controlls the custom payload after login or token refresh. This data is returned through the web API.
-    """
-    return {
-        'token': token,
-        'user': {
-        }
-    }
+#     This function controlls the custom payload after login or token refresh. This data is returned through the web API.
+#     """
+#     return {
+#         'token': token,
+#         'user': {
+#         }
+#     }
