64cf0a6518446302cbd760fb59b88c3743defa34
masontmorrow <masontmorrow@gmail.com>
2018-10-12 04:33:43 +0200

    Altered props import

--- frontend/src/components/company/CompanyJobCounter.js
+++ frontend/src/components/company/CompanyJobCounter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const CompanyJobCounter = props => {
-    const { count, stripe_count, published_count } = this.props;
+    const { count, stripe_count, published_count } = props;
     return (
         <div className="company-job-count">
             <p>Number of Job Posts saved: {count}</p>
-            <p>Number of Job Posts published: {published_count}</p>
+            {/* <p>Number of Job Posts published: {published_count}</p> */}
             <p>Number of Job Posts available: {stripe_count}</p>
         </div>
     );
