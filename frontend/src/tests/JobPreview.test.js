910596d3e57048fa385839642da7f3725c517dca
Dixie Korley <dixiekorley@gmail.com>
2018-10-13 11:35:46 -0400

    add Search, JobPreview not passing

--- frontend/src/tests/JobPreview.test.js
+++ frontend/src/tests/JobPreview.test.js
@@ -2,6 +2,5 @@
 import { JobPreview } from "../components";
 
 test('JobPreview', () => {
-    const component = shallow(<JobPreview />);
-    expect(component).toHaveLength(1);
+    shallow(<JobPreview {...props} />);
 });
--- frontend/src/tests/TagCreat.test.js
+++
@@ -1,7 +0,0 @@
-
-import { TagCreate } from "../components";
-
-test('TagCreate', () => {
-    const component = shallow(<TagCreate />);
-    expect(component).toHaveLength(1);
-});
