3b9f3f882910e65343a08259fc24aac3229a84d4
masontmorrow <masontmorrow@gmail.com>
2018-10-15 00:09:16 +0200

    Adds validators to AccountUpdate &:
    - Modal to confirm username change
    - New handlers for modals

--- frontend/src/components/utility/AccountUpdate.js
+++ frontend/src/components/utility/AccountUpdate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 import axios from 'axios';
-import { Collapse, Form, Input, Button } from 'antd';
+import { Collapse, Form, Input, Button, Alert, Modal } from 'antd';
 
 const FormItem = Form.Item;
 const Panel = Collapse.Panel;
@@ -11,7 +11,9 @@ class AccountUpdate extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            error: null,
+            message: null,
+            formModal: false
         }
     }
 
@@ -19,10 +21,105 @@ class AccountUpdate extends React.Component {
 
     }
 
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handlePasswordSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    handleEmailSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                this.setState({ error: null, message: null });
+                const token = localStorage.getItem('token');
+                // Simple token comparison
+                this.props.checkToken(e, this.props.token, token);
+                // POST to verify token; returns JWT if valid
+                axios.post(`${process.env.REACT_APP_LOGIN_API}verify/`, {token: token})
+                .then(response => {
+                    this.toggleFormModal();
+                })
+                .catch(err => {
+                    this.props.logOut(e, err);
+                });
+            }
+        });
+    }
+
+    postEmail = (e) => {
+        const token = localStorage.getItem('token');
+        const requestOptions = { headers: { Authorization: `JWT ${token}`}};
+        const password = document.getElementById("username_reset_password").value;
+        const { email } = this.state;
+        console.log(email, password);
+        const formData = new FormData();
+        formData.append('email', email);
+        axios.patch(`${process.env.REACT_APP_API}account/${this.props.user}/`, formData, requestOptions)
+            .then(response => {
+                this.setState({ message: `Account successfully updated!`});
+                axios.post(`${process.env.REACT_APP_LOGIN_API}`, { email: email, password: password})
+                    .then(response => {
+                        localStorage.setItem('token', response.data.token);
+                        this.toggleFormModal();
+                    })
+                    .catch(err => {
+                        this.setState({ error: `Error loggin in. Log in again.`});
+                        this.toggleFormModal();
+                    });
+            })
+            .catch(err => {
+                this.setState({ error: `Error processsing your request.`});
+            });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Paswords do not match.');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+            callback();
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+
+    toggleFormModal = () => {
+        this.setState({ formModal: !this.state.formModal });
+    }
+
+
     render() {
         const { getFieldDecorator } = this.props.form;
+        const { error, message, formModal } = this.state;
         return (
         <div className="form flex column account-change">
+
+            {error ? (
+              <Alert message={error} type="error" closable showIcon  onClose={this.resetMessages}/>
+              ) : (null)}
+            {message ? (
+              <Alert message={message} type="success" closable showIcon onClose={this.resetMessages}/>
+            ) : (null)}
+
             <Collapse className="flex column" bordered={false} accordion={true}>
           
                 <Panel className="h4" header="Update Password" key="1">
@@ -36,7 +133,7 @@ class AccountUpdate extends React.Component {
                           validator: this.validateToNextPassword,
                         }],
                       })(
-                        <Input type="password" />
+                        <Input type="password" onChange={this.onChange} name="password"/>
                       )}
                     </FormItem>
 
@@ -48,7 +145,7 @@ class AccountUpdate extends React.Component {
                           validator: this.compareToFirstPassword,
                         }],
                       })(
-                        <Input type="password" onBlur={this.handleConfirmBlur} />
+                        <Input type="password" onBlur={this.handleConfirmBlur} onChange={this.onChange} name="confirm"/>
                       )}
                     </FormItem>
 
@@ -69,9 +166,21 @@ class AccountUpdate extends React.Component {
                           required: false, message: 'Please input your email!',
                         }],
                       })(
-                        <Input />
+                        <Input onChange={this.onChange} name="email"/>
                       )}
                     </FormItem>
+
+                    <Modal title="Enter your password"
+                    visible={formModal}
+                    onCancel={this.toggleFormModal}
+                    footer={[null, null,]} >
+
+                        <FormItem label="password">
+                            <Input type="password" id="username_reset_password"/>
+                            <Button onClick={this.postEmail}>Submit</Button>
+                        </FormItem>
+
+                    </Modal>
                     
                     <Button type="primary" onClick={this.handleEmailSubmit}>Change Email</Button>
 
