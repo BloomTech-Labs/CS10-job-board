f8d672e13be87c1c174eef9bffc5a54f7a20f9ef
masontmorrow <masontmorrow@gmail.com>
2018-10-14 21:21:45 +0200

    Extends custom configuration of UserView:
    - Rejects all PUT requests
    - Does not return data for security reasons
    - GET request must be reworked to remove password

--- jobs/views.py
+++ jobs/views.py
@@ -100,6 +100,49 @@ class UserView(generics.RetrieveUpdateDestroyAPIView):
         id = self.request.user.pk
         return User.objects.filter(id=id)
 
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+        # Check job company id matches user id
+        if user.pk is not self.request.user.pk:
+            message = {'FORBIDDEN'}
+            return Response(message, status=status.HTTP_403_FORBIDDEN)
+
+        partial = kwargs.pop('partial', False)
+        if partial is False:
+            message = { 'FORBIDDEN' }
+            return Response(message, status=status.HTTP_403_FORBIDDEN)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.get_object()
+        # Checks user id on request == user id making delete request:
+        #   (prevents company 1 deleting for company 2)
+        if user.pk is not self.request.user.pk:
+            message = {'FORBIDDEN'}
+            return Response(message, status=status.HTTP_403_FORBIDDEN)
+        self.perform_destroy(user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def perform_destroy(self, instance):
+        instance.delete()
+        
 
 # Resets the jwt_secret, invalidating all token issued
 class UserLogoutAllView(views.APIView):
