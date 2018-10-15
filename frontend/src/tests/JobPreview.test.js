c94751a7d548670342e2d7db6dc4feaa25a10fd0
Dixie Korley <dixiekorley@gmail.com>
2018-10-15 13:55:55 -0400

    test for TagView

--- frontend/src/tests/JobPreview.test.js
+++ frontend/src/tests/JobPreview.test.js
@@ -1,13 +1,7 @@
-import { JobPreview, StylizedLine } from '../components';
-
+import { JobPreview } from '../components';
 
 test('JobPreview', () => {
 	const component = mount(<JobPreview job={{}} />);
     expect(component.props().job).toBeDefined();
     expect(component.find('StylizedLine'));
 });
-
-// test('StylizedLine', () => {
-// 	const component = renderer.create(<StylizedLine />).toJSON();
-// 	expect(component).toMatchSnapshot();
-// });
