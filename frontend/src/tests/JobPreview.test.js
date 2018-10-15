52977b93bc07722da49828b26a634f58c70532cc
Dixie Korley <dixiekorley@gmail.com>
2018-10-15 13:27:08 -0400

    solve stateless component shallowing rendering test

--- frontend/src/tests/JobPreview.test.js
+++ frontend/src/tests/JobPreview.test.js
@@ -2,11 +2,17 @@
 import { JobPreview } from "../components";
 
 test('JobPreview', () => {
-    const mockProps = {
-        title: "firstTitle",
-        min_salary: 100000,
-        max_salary: 200000,
-        description: "firstDescription"
-    }
-    shallow(<JobPreview {...mockProps} />);
+    // const job = {
+    //     title: 'Software Engineer',
+    //     min_salary: 100000,
+    //     max_salary: 150000,
+    //     description: 'Software engineer description'
+    // }
+    // const component = shallow(<JobPreview job="post" />);
+    // expect(component.props().job).toEqual();
+    // expect(JobPreview()).toContainEqual(job);
+    // const component = shallow(<JobPreview job="post" />);
+   const component = mount(<JobPreview job={{}} />); 
+   expect(component.props().job).toBeDefined();
+    
 });
