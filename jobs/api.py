3901353ce158c6a2e5aa25581adf0b9540618aa6
masontmorrow <masontmorrow@gmail.com>
2018-10-15 19:37:27 +0200

    Add custom update() on UserViewSeriazlier & import helpers

--- jobs/api.py
+++ jobs/api.py
@@ -1,6 +1,10 @@
 from .models import JobPost, User, UserMembership, Membership, Payment
 from rest_framework import serializers
+# Customization of UserSerializer
+from rest_framework.utils import model_meta
+from .seralizer_helpers import raise_errors_on_nested_writes
 from django.contrib.auth.hashers import make_password
+# Tag serializer for JobPostSerializer
 from taggit_serializer.serializers import (TagListSerializerField, TaggitSerializer)
 
  
@@ -12,17 +16,11 @@ class JWTSerializer(serializers.ModelSerializer):
         fields = ('id', 'is_employer')
 
 
-# Used by Djoser to register users, references in settings.py / DJOSER.SERIALIZERS.user_create
 class UserRegistrationSerializer(serializers.ModelSerializer):
 
     # Encrypts password with create_user=Django default create user method
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        if 'password' in validated_data:
-            user.password = make_password(
-                validated_data.get('password')
-            )
-            user.save()
         return user
 
     class Meta:
@@ -38,14 +36,31 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 class UserViewSerializer(serializers.ModelSerializer):
 
-    def update(self, instance, validated_data):
+    def update(self, user, validated_data):
+        raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(user)
+
         if 'password' in validated_data:
-            instance.password = make_password(
+            user.password = make_password(
                 validated_data.get('password')
             )
-            instance.save()
+            user.save()
+            return user
+        elif 'email' in validated_data:
+            user.email = validated_data.get('email')
+            user.save()
+            return user
 
-        return instance
+        # From: Django Serializers: https://github.com/encode/django-rest-framework/blob/master/rest_framework/serializers.py#L969
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                field = getattr(user, attr)
+                field.set(value)
+            else:
+                setattr(user, attr, value)
+        user.save()
+
+        return user
 
     class Meta:
         model = User
