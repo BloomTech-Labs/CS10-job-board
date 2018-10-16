ffe57a97d325c6f1de92573af59e0a58b60ecfa5
Dixie Korley <dixiekorley@gmail.com>
2018-10-15 20:42:59 -0400

    add test for UserManager, no errors

--- jobs/tests/test_models.py
+++ jobs/tests/test_models.py
@@ -1,6 +1,6 @@
 
 from django.utils import timezone
-from jobs.models import JobPost
+from jobs.models import JobPost, UserManager
 import pytest
  
 # JobPost Test Suite
@@ -38,18 +38,84 @@ class TestJobPost:
         def test_str(self):
             assert self.__str__ == self.title
 
+
+# UserManager Test Suite
+class UserManagerTest:
+    def test__create_user(self):
+        testUserManager = UserManager()
+        assert testUserManager._create_user == testUserManager.user
+
+    def test_create_user(self):
+        testUserManager = UserManager()
+        assert testUserManager.create_user == testUserManager._create_user
+
+    def test_create_superuser(self):
+        testUserManager = UserManager()
+        assert testUserManager._create_user == testUserManager._create_user
+
+
 """
-guide for test cases
-company_name = models.ForeignKey('jobs.User', on_delete=models.CASCADE)
-title = models.CharField(max_length=200)
-description = models.TextField()
-job_location = models.CharField(max_length=30, blank=True)
-requirements = models.TextField()
-min_salary = models.IntegerField(null=True, blank=True)
-max_salary = models.IntegerField(null=True, blank=True)
-is_active = models.BooleanField(default=False)
-tags = TaggableManager(
-    verbose_name="Tags", help_text="Enter tags separated by commas", blank=True)
-created_date = models.DateTimeField(default=timezone.now)
-published_date = models.DateTimeField(blank=True, null=True)
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+    
+        Create and save a user with the given email and password.
+    
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        is_employee = models.BooleanField(default=False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 """
+
+
+# class User(AbstractBaseUser, PermissionsMixin):
+#     is_employer = models.BooleanField(default=False)
+#     email = models.EmailField(
+#         verbose_name='email address', max_length=255, unique=True)
+#     password = models.CharField(max_length=100, default="", null=False)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+#     company_name = models.CharField(max_length=200, blank=True)
+#     company_logo = models.FileField(
+#         upload_to='company_logo/%Y/%m/%d/', blank=True, null=True)
+#     company_summary = models.CharField(max_length=6000, blank=True)
+#     application_inbox = models.EmailField(blank=True, default='')
+#     first_name = models.CharField(max_length=200, blank=True)
+#     last_name = models.CharField(max_length=200, blank=True)
+#     profile_photo = models.ImageField(
+#         upload_to='profile_photo/%Y/%m/%d/', blank=True, null=True)
+#     created_date = models.DateTimeField(default=timezone.now, editable=False)
+#     jwt_secret = models.UUIDField(default=uuid.uuid4)
+
+#     objects = UserManager()
+
+#     USERNAME_FIELD = 'email'
+#     # REQUIRED_FIELDS = []
+
+#     class Meta:
+#         ordering = ['created_date']
+
+#     def __str__(self):
+#         return self.email
