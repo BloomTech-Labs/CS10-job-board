068865322bb74c544a08fabb06a4d92abd7213f8
Dixie Korley <dixiekorley@gmail.com>
2018-10-16 11:53:42 -0400

    test for UserManager and User, debug error

--- jobs/tests/test_models.py
+++ jobs/tests/test_models.py
@@ -1,6 +1,6 @@
 
 from django.utils import timezone
-from jobs.models import JobPost, UserManager
+from jobs.models import JobPost, UserManager, User
 import pytest
  
 # JobPost Test Suite
@@ -40,7 +40,7 @@ class TestJobPost:
 
 
 # UserManager Test Suite
-class UserManagerTest:
+class TestUserManager:
     def test__create_user(self):
         testUserManager = UserManager()
         assert testUserManager._create_user == testUserManager.user
@@ -53,69 +53,68 @@ class UserManagerTest:
         testUserManager = UserManager()
         assert testUserManager._create_user == testUserManager._create_user
 
+class TestUser:
+    def test_user_model(self):
+        self.is_employer = True
+        self.email = 'example_email@gmail.com'
+        self.password = 'example_password'
+        self.is_active = True
+        self.is_staff = True
+        self.company_name = 'Example Company'
+        self.company_logo = '<img src=\'#\' alt=\'example\'>'
+        self.company_description = "An example company description for the company"
+        self.application_inbox = 'javascript, mongodb, sql, express.js'
+        self.first_name = "Example"
+        self.profile_photo = '<img src=\'#\' alt=\'example\'>'
+        self.created_date = timezone.now()
+        self.jwt_secret = '{12345678-1234-5678-1234-567812345678}'
 
+        self.test_user = User.objects.create(
+            is_employer = self.is_employer,
+            email = self.email,
+            password = self.password,
+            is_active = self.is_active,
+            is_staff = self.is_staff,
+            company_name = self.company_name,
+            company_logo = self.company_logo,
+            company_description = self.company_description,
+            application_inbox = self.application_inbox,
+            first_name = self.first_name,
+            profile_photo = self.profile_photo,
+            created_date = self.created_date,
+            jwt_secret = self.jwt_secret,
+        )
+
+        def test_str(self):
+            assert self.__str__ == self.email
 """
-class UserManager(BaseUserManager):
+class User(AbstractBaseUser, PermissionsMixin):
+    is_employer = models.BooleanField(default=False)
+    email = models.EmailField(
+        verbose_name='email address', max_length=255, unique=True)
+    password = models.CharField(max_length=100, default="", null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    company_name = models.CharField(max_length=200, blank=True)
+    company_logo = models.FileField(
+        upload_to='company_logo/%Y/%m/%d/', blank=True, null=True)
+    company_summary = models.CharField(max_length=6000, blank=True)
+    application_inbox = models.EmailField(blank=True, default='')
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    profile_photo = models.ImageField(
+        upload_to='profile_photo/%Y/%m/%d/', blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
+    jwt_secret = models.UUIDField(default=uuid.uuid4)
 
-    use_in_migrations = True
+    objects = UserManager()
 
-    def _create_user(self, email, password, **extra_fields):
-    
-        Create and save a user with the given email and password.
-    
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = []
 
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        is_employee = models.BooleanField(default=False)
-        return self._create_user(email, password, **extra_fields)
+    class Meta:
+        ordering = ['created_date']
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
+    def __str__(self):
+        return self.email
 """
-
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     is_employer = models.BooleanField(default=False)
-#     email = models.EmailField(
-#         verbose_name='email address', max_length=255, unique=True)
-#     password = models.CharField(max_length=100, default="", null=False)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#     company_name = models.CharField(max_length=200, blank=True)
-#     company_logo = models.FileField(
-#         upload_to='company_logo/%Y/%m/%d/', blank=True, null=True)
-#     company_summary = models.CharField(max_length=6000, blank=True)
-#     application_inbox = models.EmailField(blank=True, default='')
-#     first_name = models.CharField(max_length=200, blank=True)
-#     last_name = models.CharField(max_length=200, blank=True)
-#     profile_photo = models.ImageField(
-#         upload_to='profile_photo/%Y/%m/%d/', blank=True, null=True)
-#     created_date = models.DateTimeField(default=timezone.now, editable=False)
-#     jwt_secret = models.UUIDField(default=uuid.uuid4)
-
-#     objects = UserManager()
-
-#     USERNAME_FIELD = 'email'
-#     # REQUIRED_FIELDS = []
-
-#     class Meta:
-#         ordering = ['created_date']
-
-#     def __str__(self):
-#         return self.email
