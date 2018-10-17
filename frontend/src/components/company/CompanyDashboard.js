b1258b674141e01fbba9df6a331206cfca0dd719
masontmorrow <masontmorrow@gmail.com>
2018-10-17 13:45:02 +0200

    Revert CompanyDashboard

--- frontend/src/components/company/CompanyDashboard.js
+++ frontend/src/components/company/CompanyDashboard.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { withRouter, NavLink } from 'react-router-dom';
 import axios from 'axios';
-import { CompanyJobCounter, CompanyJobList } from '../';
+import { CompanyJobList, CompanyJobBalance } from '../';
 
 class CompanyDashboard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
         }
     }
 
-    setJobCount = count => {
-        this.setState({ count: count });
-    }
-
     render() {
+        const { count, published_count } = this.state;
         return (
             <div className="dashboard">
                 <CompanyJobList />
-                <CompanyJobCounter count={count} stripe_count={stripe_count} published_count={published_count}/>
+                <CompanyJobBalance />
             </div>
         );
     }
