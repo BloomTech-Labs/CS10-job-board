a210ebe50b2620cc0edf262343e3fcc0865e10e4
masontmorrow <masontmorrow@gmail.com>
2018-10-17 14:14:22 +0200

    Revert CompanyJobCounter to a stateless component

--- frontend/src/components/company/CompanyJobCounter.js
+++ frontend/src/components/company/CompanyJobCounter.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import axios from "axios";
 
-class CompanyJobCounter extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            stripe_count: null
-        }   
-    }
-
-    componentDidMount() {
-        // axios call to Stripe API asking for how many jobs purchased or what is left
-    }
-
-    render() {
-        const { count, published_count } = this.props;
-        const { stripe_count } = this.state;
-        return (
-            <div className="company-job-count">
-                <p>Number of Job Posts saved: {count}</p>
-                <p>Number of Job Posts published: {published_count}</p>
-                <p>Number of Job Posts available: {stripe_count}</p>
-            </div>
-        );
-    }    
+const CompanyJobCounter = props => {
+    const { count, published_count } = props;
+    return (
+        <div className="company-job-count">
+            <p>Number of Job Posts saved: {count}</p>
+            <p>Number of Job Posts published: {published_count}</p>
+        </div>
+    );
 }
 
 export default CompanyJobCounter;
