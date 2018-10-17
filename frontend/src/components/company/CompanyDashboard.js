738b0aa9eba645a44d4a8e2a5212721c8493c4fc
masontmorrow <masontmorrow@gmail.com>
2018-10-17 03:36:16 +0200

    Add a setJobCount() on CompanyDashboard

--- frontend/src/components/company/CompanyDashboard.js
+++ frontend/src/components/company/CompanyDashboard.js
@@ -12,12 +12,9 @@ class CompanyDashboard extends Component {
     }
     render() {
         return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6"></div>
-                    <div className="col s12 m5 offset-m1"></div>
-                    <CompanyJobList />
-                </div>
+            <div className="dashboard">
+                <CompanyJobList />
+                <CompanyJobCounter />
             </div>
         );
     }
