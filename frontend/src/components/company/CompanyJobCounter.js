c71e54b32b4b00cff7ffa4c2855266cfe4540af9
masontmorrow <masontmorrow@gmail.com>
2018-10-17 03:44:30 +0200

    Convert CompanyJobCounter into a class

--- frontend/src/components/company/CompanyJobCounter.js
+++ frontend/src/components/company/CompanyJobCounter.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import axios from "axios";
 
-const CompanyJobCounter = props => {
-    const { count, stripe_count, published_count } = props;
-    return (
-        <div className="company-job-count">
-            <p>Number of Job Posts saved: {count}</p>
-            {/* <p>Number of Job Posts published: {published_count}</p> */}
-            <p>Number of Job Posts available: {stripe_count}</p>
-        </div>
-    );
+class CompanyJobCounter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            stripe_count: null
+        }   
+    }
+
+    componentDidMount() {
+        // axios call to Stripe API asking for how many jobs purchased or what is left
+    }
+
+    render() {
+        const { count, published_count } = this.props;
+        const { stripe_count } = this.state;
+        return (
+            <div className="company-job-count">
+                <p>Number of Job Posts saved: {count}</p>
+                <p>Number of Job Posts published: {published_count}</p>
+                <p>Number of Job Posts available: {stripe_count}</p>
+            </div>
+        );
+    }    
 }
+
 export default CompanyJobCounter;
