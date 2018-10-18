1ea801e811699708ec3ab60a5824edee45ce8545
Dixie Korley <dixiekorley@gmail.com>
2018-10-18 13:48:54 -0400

    add 'account' path test

--- jobs/tests/test_urls.py
+++ jobs/tests/test_urls.py
@@ -1,8 +1,14 @@
 from django.urls import resolve, Resolver404
 
 class TestJobBoardUrls:
-    def test_root_url(self):
+    def test_register_url(self):
         try:
             path = resolve('api/register')
         except Resolver404:
-            'Not found'
+            " `api/register` not found"
+
+    def test_account_url(self):
+        try:
+            path = resolve('api/account/<int:pk>/')
+        except Resolver404:
+                "`api/account/<int:pk>` not found"
