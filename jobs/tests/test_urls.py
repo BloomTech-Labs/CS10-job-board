844137ae053e0e1e656c6fcbdc9a6132f1014f9e
Dixie Korley <dixiekorley@gmail.com>
2018-10-18 13:54:23 -0400

    add test for auth routes

--- jobs/tests/test_urls.py
+++ jobs/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.urls import resolve, Resolver404
 
-class TestJobBoardUrls:
+class TestUrls:
     def test_register_url(self):
         try:
             path = resolve('api/register')
@@ -12,3 +12,27 @@ class TestJobBoardUrls:
             path = resolve('api/account/<int:pk>/')
         except Resolver404:
                 "`api/account/<int:pk>` not found"
+
+    def test_login_url(self):
+        try:
+            path = resolve('api/login/')
+        except Resolver404:
+                "`api/login` not found"
+
+    def test_login_refresh_url(self):
+        try:
+            path = resolve('api/login/refresh/')
+        except Resolver404:
+                "`api/login/refresh/` not found"
+
+    def test_login_verify_url(self):
+        try:
+            path = resolve('api/login/verify/')
+        except Resolver404:
+                "`api/login/verify/` not found"
+
+    def test_logout_all_url(self):
+        try:
+            path = resolve('api/logout/all/')
+        except Resolver404:
+                "`api/logout/all/` not found"
