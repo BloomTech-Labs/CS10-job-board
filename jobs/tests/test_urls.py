126df2117f6580543eea37686c734f734940bbae
Dixie Korley <dixiekorley@gmail.com>
2018-10-18 13:44:12 -0400

    test for api/register

--- jobs/tests/test_urls.py
+++ jobs/tests/test_urls.py
@@ -1,2 +1,8 @@
-from django.urls import reverse, resolve
+from django.urls import resolve, Resolver404
 
+class TestJobBoardUrls:
+    def test_root_url(self):
+        try:
+            path = resolve('api/register')
+        except Resolver404:
+            'Not found'
