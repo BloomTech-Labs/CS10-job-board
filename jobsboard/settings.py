f20496bcddf05fd07fe1baa2e7eb1d7b32a61bbd
masontmorrow <masontmorrow@gmail.com>
2018-10-18 22:58:31 +0200

    Remove default pagination class:
    - To reduce data sent from API's not needing pagination

--- jobsboard/settings.py
+++ jobsboard/settings.py
@@ -166,9 +166,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    )
 }
 
 # API slash addition settings: False means it will not add an extra /,
