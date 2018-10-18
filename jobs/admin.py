3855f6e64d64bdbd9c4078a15b84b458ab7c0056
masontmorrow <masontmorrow@gmail.com>
2018-10-18 20:01:53 +0200

    Remove unused models from admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-# from jobs.models import JobPost, Employee, Employer, User, Membership, UserMembership, Subscription
-from jobs.models import JobPost, User, Membership, UserMembership, Subscription
+from jobs.models import JobPost, User, UserMembership, Subscription
 
 admin.site.register(JobPost)
-# admin.site.register(Employee)
-# admin.site.register(Employer)
 admin.site.register(User)
-admin.site.register(Membership)
 admin.site.register(UserMembership)
 admin.site.register(Subscription)
