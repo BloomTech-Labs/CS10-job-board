ec4b3fbe450adb4660d265b7a4ecbafe3e99a1ad
masontmorrow <masontmorrow@gmail.com>
2018-10-20 05:13:56 +0200

    Update CompanyJobCounter to render new data

--- frontend/src/components/company/CompanyJobCounter.js
+++ frontend/src/components/company/CompanyJobCounter.js
@@ -2,11 +2,11 @@ import React from 'react';
 import axios from "axios";
 
 const CompanyJobCounter = props => {
-    const { count, published_count } = props;
+    const { count, published_count, unpublished_count } = props;
     return (
-        <div className="company-job-count">
-            <p>Number of Job Posts saved: {count}</p>
-            <p>Number of Job Posts published: {published_count}</p>
+        <div className="company-job-count flex justify-space-between">
+            <p>Published: {published_count}</p>
+            <p>Drafts: {unpublished_count}</p>
         </div>
     );
 }
