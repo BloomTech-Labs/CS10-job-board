4d2d2267da030225b535f78f7c19107ba816cbf1
masontmorrow <masontmorrow@gmail.com>
2018-10-21 18:33:48 +0200

    Register Payment to admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from jobs.models import JobPost, User, UserMembership, Subscription
+from jobs.models import JobPost, User, UserMembership, Subscription, Payment
 
 admin.site.register(JobPost)
 admin.site.register(User)
 admin.site.register(UserMembership)
 admin.site.register(Subscription)
+admin.site.register(Payment)
