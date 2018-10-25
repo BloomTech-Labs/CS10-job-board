a219abf68fec5906d8e9dc7dfcc499333d12c0e0
masontmorrow <masontmorrow@gmail.com>
2018-10-25 23:46:25 +0200

    Convert description to ul / li's, & import Button from ant

--- frontend/src/components/billing/Billing.js
+++ frontend/src/components/billing/Billing.js
@@ -3,7 +3,7 @@ import { CSSTransition } from "react-transition-group";
 import { withRouter } from 'react-router-dom';
 import { Elements, StripeProvider} from 'react-stripe-elements';
 import {CheckoutForm} from '../';
-import { Icon } from 'antd';
+import { Icon, Button } from 'antd';
 
 class Billing extends React.Component {
     state = {
@@ -16,9 +16,9 @@ class Billing extends React.Component {
         const titles = [ `1 Job`, `12 Jobs`, `Unlimited Jobs`];
         const prices = [ 9.99, 99.99, 299.99 ]
         const descriptions = [
-            `A single job post credit, published for 30 days. If unpublished after publishing, you can re-publish the job again until the 30 days is used.`,
-            `12 job credits, published for 30 days. If unpublished after publishing, you can re-publish the job again until the 30 days is used.`,
-            `Unlimited posting and publishing. Post as many jobs for as long as you wish. Publish & unpublish jobs as needed.`
+            [`1 job post credit, published for 30 days.`, `If unpublished after publishing, you can re-publish the job again until the 30 days is used.`],
+            [`12 job credits, published for 30 days.`, `If unpublished after publishing, you can re-publish the job again until the 30 days is used.`],
+            [`Unlimited posting and publishing.`, `Post as many jobs for as long as you wish. Publish & unpublish jobs as needed.`]
         ]
         const skus = [`sku_DoNhM1EGgKGLeg`, `sku_DoNp2frdbkieqn`, `plan_DoNu8JmqFRMrze`]
         for (let i = 0; i < titles.length; i++) {
@@ -45,17 +45,22 @@ class Billing extends React.Component {
     render() {
         const { products, activeProduct } = this.state;
         const active = activeProduct !== null;
-        console.log(active);
         const CheckoutCard = props => (
             <div className="checkout-card">
                 <h1>{props.title}</h1>
                 <h3>{`$${props.price}`}</h3>
-                <p>{props.description}</p>
+                <ul>
+                    {props.description.map((item, i) => {
+                        return (
+                            <li key={i}>{item}</li>
+                        )
+                    })}
+                </ul>
                 <div className="whitespace"></div>
                 {props.active ? (
                    <Icon type="check-circle" />
                 ) : (
-                    <button onClick={props.hideOthers} name={`${props.sku}`}>Select</button>
+                    <Button type="primary" onClick={props.hideOthers} name={`${props.sku}`}>Select</Button>
                 )}
             </div>
         );
