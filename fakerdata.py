cd72b51bd1fd8328ee35b08627633e104e8cde60
masontmorrow <masontmorrow@gmail.com>
2018-11-13 11:28:23 +0100

    Add UserPaymentFactory to fakerdata.py

--- fakerdata.py
+++ fakerdata.py
@@ -1,6 +1,5 @@
 import factory
 from jobs import models
-from random import randint, choice
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -15,16 +14,26 @@ class JobPostFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.JobPost
 
-    company = 'company' 
-    company_name = 'company_name' 
-    title = 'title' 
-    company_logo = 'company_logo' 
-    description = 'description' 
-    job_location = 'job_location' 
-    requirements = 'requirements' 
-    min_salary = 'min_salary' 
+    company = 'company'
+    company_name = 'company_name'
+    title = 'title'
+    company_logo = 'company_logo'
+    description = 'description'
+    job_location = 'job_location'
+    requirements = 'requirements'
+    min_salary = 'min_salary'
     max_salary = 'max_salary'
-    is_active = 'is_active' 
-    tags = 'tags' 
-    created_date = 'created_date' 
+    is_active = 'is_active'
+    tags = 'tags'
+    created_date = 'created_date'
     published_date = 'published_date'
+
+
+class UserPaymentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.UserPayment
+
+    purchased = 'purchased'
+    quantity = 'quantity'
+
+    
